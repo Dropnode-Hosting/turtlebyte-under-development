eb3cc04bbafaf0d87368ef22557f9b80a0946890
Turlx <arussellaaron@gmail.com>
2023-02-06 16:28:27 -0800

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Under Development</h1>
+    <img src="/logo.png" alt="Turtlebyte Logo" />
+    <p>If you are interested in game server hosting now please check out our other host at <a href="https://dropnode.net">https://dropnode.net</a></p>
+    <p>For basic front-end web development you can find my info in my portfolio at <a href="https://turlxthehuman.com">https://turlxthehuman.com</a> this will be taking up most of my time as well as other projetcs, so I will not be offering fullstack development at the moment.</p>
+  </div>
+`
+
